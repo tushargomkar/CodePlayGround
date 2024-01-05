eb74ded3d25ff22ad427fbeae9bcee5a3ed7d06c
tushargomkar <tgomkar5@gmail.com>
2024-01-05 11:06:24 +0530

    Problems add in the js basic

---
+++ JS/HARKIRAT-JS/1.js
@@ -0,0 +1,15 @@
+let a=[1,2,3,4,5,12,2,2,1,1,1]
+const n =a.length
+let sum=0
+for(let i=0;i<n; i++){
+    if(a[i]%2 ==0){
+        sum+=a[i];
+    }
+}
+
+console.log(sum)
+console.log(sum)
+console.log(sum)
+console.log(sum)
+console.log(sum)
+console.log(sum)
---
+++ JS/HARKIRAT-JS/problem.js
@@ -0,0 +1,11 @@
+
+let start=performance.now()
+const add=setTimeout(sum,5000)
+
+
+
+function sum(){
+    let end=performance.now()
+    console.log(end-start);
+    return 5+6
+}
