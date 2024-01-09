61f3979e401a1f595bc97d1975346438b5dbc621
Tushar Gomkar <tgomkar5@gmail.com>
2024-01-09 10:36:19 +0530

    Update 1.anagram

--- JS/HARKIRAT-JS/1.js
+++ JS/HARKIRAT-JS/1.js
@@ -1,15 +1,33 @@
-let a=[1,2,3,4,5,12,2,2,1,1,1]
-const n =a.length
-let sum=0
-for(let i=0;i<n; i++){
-    if(a[i]%2 ==0){
-        sum+=a[i];
-    }
-}
+function checkAnagramWithSort(str1, str2) {
+    let newStr1 = str1.toLowerCase().replace(/[^a-z]/g, '')
+    let newStr2 = str2.toLowerCase().replace(/[^a-z]/g, '')
 
-console.log(sum)
-console.log(sum)
-console.log(sum)
-console.log(sum)
-console.log(sum)
-console.log(sum)
+    if (newStr1.length !== newStr2.length) {
+      return false
+    }
+
+    let hash1 = {}
+    let hash2 = {}
+
+    newStr1.split('').map(letter => {
+      hash1[letter] ? hash1[letter]++ : hash1[letter] = 1
+    })
+
+    newStr2.split('').map(letter => {
+      hash2[letter] ? hash2[letter]++ : hash2[letter] = 1
+    })
+
+    let hash1keys = Object.keys(hash1)
+    let hash2keys = Object.keys(hash2)
+
+    for (let i = 0; i<hash1keys.length; i++) {
+      if (!hash2keys.includes(hash1keys[i])) {
+        return false
+      }
+      if (hash1[hash1keys[i]] !== hash2[hash1keys[i]]) {
+        return false
+      }
+    }
+
+    return true
+  }
