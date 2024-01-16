6b73947a4b30259a751b346b6639c0e051a5e658
tushargomkar <tgomkar5@gmail.com>
2024-01-16 21:39:10 +0530

    promise

--- JS/HARKIRAT-JS/1.js
+++ JS/HARKIRAT-JS/1.js
@@ -1,15 +1,5 @@
-let a=[1,2,3,4,5,12,2,2,1,1,1]
-const n =a.length
-let sum=0
-for(let i=0;i<n; i++){
-    if(a[i]%2 ==0){
-        sum+=a[i];
-    }
+function hasDone(){
+    console.log("here after the");
 }
 
-console.log(sum)
-console.log(sum)
-console.log(sum)
-console.log(sum)
-console.log(sum)
-console.log(sum)
+setTimeout(hasDone,1000)
